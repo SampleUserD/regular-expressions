ceaf8e5b2e901f7589452ae4c145813c815d7d88
SampleUserD <61961965+SampleUserD@users.noreply.github.com>
2023-04-24 19:34:53 +0500

    #3.3 [Executor library became more generic]

--- _libraries/executor/Command.py
+++ _libraries/executor/Command.py
@@ -3,7 +3,8 @@ from typing import Generic, TypeVar
 from .Dependency import Dependency
 
 Dependencies = TypeVar('Dependencies', bound=Dependency)
+ReturnType = TypeVar('ReturnType')
 
 class Command(Generic[Dependencies]):
-  def execute(self, dependencies: Dependencies) -> None:
+  def execute(self, dependencies: Dependencies) -> ReturnType:
     raise Exception('You should implement Command.execute(Context)')
--- _libraries/executor/__init__.py
+++ _libraries/executor/__init__.py
@@ -1,3 +1,4 @@
 from .Command import Command as Command
 from .Dependency import Dependency as Dependency
 from .Executor import Executor as Executor
+from .ImmediateExecutor import ImmediateExecutor as ImmediateExecutor
